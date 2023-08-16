ccac66b6b248d7c1de4fdf4b2b81a61c911dba30
Walmyr <wlsf82@gmail.com>
2023-08-16 22:48:12 +0200

    Solve the first extra exercise of lesson 2

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -8,10 +8,12 @@ describe('TAT Customer Service Center', () => {
   })
 
   it('fills in the required fields and submits the form', () => {
+    const longText = Cypress._.repeat('abcdefghijklmnopqrstuvwxyz', 10)
+
     cy.get('#firstName').type('Walmyr')
     cy.get('#lastName').type('Lima e Silva Filho')
     cy.get('#email').type('walmyr@talkingabouttesting.com')
-    cy.get('#open-text-area').type('Thanks!')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
     cy.get('button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
