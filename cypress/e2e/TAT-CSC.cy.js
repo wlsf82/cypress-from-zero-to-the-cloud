1660ed266bf6ab3a8c2f21418e8d406a48a3bc44
Walmyr <wlsf82@gmail.com>
2023-08-16 22:39:14 +0200

    Write the first test and update npm scripts

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('TAT Customer Service Center', () => {
+  it('check application title', () => {
+    cy.visit('./src/index.html')
+
+    cy.title().should('be.equal', 'TAT Customer Service Center')
   })
 })
