f35beec9f483052a0b91fa9cf09aa0342686d3a4
Walmyr <wlsf82@gmail.com>
2023-08-17 00:27:08 +0200

    Solve the second extra exercise of lesson 6

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -139,4 +139,13 @@ describe('TAT Customer Service Center', () => {
         expect(input[0].files[0].name).to.equal('example.json')
       })
   })
+
+  it('selects a file using a fixture to which an alias was given', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('input[type="file"]')
+      .selectFile('@sampleFile')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
 })
