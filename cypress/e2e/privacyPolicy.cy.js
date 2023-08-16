d3c21ed9ddfcc6005ef513c94bacc8114c8adbca
Walmyr <wlsf82@gmail.com>
2023-08-17 00:33:58 +0200

    Solve the second extra exercise of lesson 7

---
+++ cypress/e2e/privacyPolicy.cy.js
@@ -0,0 +1,6 @@
+it('independently test the privacy policy page', () => {
+  cy.visit('./src/privacy.html')
+
+  cy.contains('h1', 'TAT CSC - Privacy Policy').should('be.visible')
+  cy.contains('p', 'Talking About Testing').should('be.visible')
+})
