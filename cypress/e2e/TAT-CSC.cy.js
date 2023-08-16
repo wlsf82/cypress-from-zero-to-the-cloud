1e530365b6a999146a5671b1c57d9f4ed3821f41
Walmyr <wlsf82@gmail.com>
2023-08-16 22:44:05 +0200

    Solve the first exercise of lesson 2
    This commit moves cy.visit() to the beforeEach hook to avoid code
    duplication, and it also create a test to check that the form can
    be successfully submitted when filling in all the required fields
    and clicking the send button.

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -1,7 +1,19 @@
 describe('TAT Customer Service Center', () => {
-  it('check application title', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
 
+  it('check application title', () => {
     cy.title().should('be.equal', 'TAT Customer Service Center')
+  })
+
+  it('fills in the required fields and submits the form', () => {
+    cy.get('#firstName').type('Walmyr')
+    cy.get('#lastName').type('Lima e Silva Filho')
+    cy.get('#email').type('walmyr@talkingabouttesting.com')
+    cy.get('#open-text-area').type('Thanks!')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
   })
 })
