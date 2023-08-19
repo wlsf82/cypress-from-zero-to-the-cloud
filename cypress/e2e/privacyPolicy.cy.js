4d82b1100fb541cdd51d35c98a5ea861f62b4caf
Walmyr <wlsf82@gmail.com>
2023-08-19 21:41:22 +0200

    Solve the first extra exercise of lesson 12

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,6 +1,8 @@
-it('independently test the privacy policy page', () => {
-  cy.visit('./src/privacy.html')
+Cypress._.times(5, () => {
+  it('independently test the privacy policy page', () => {
+    cy.visit('./src/privacy.html')
 
-  cy.contains('h1', 'TAT CSC - Privacy Policy').should('be.visible')
-  cy.contains('p', 'Talking About Testing').should('be.visible')
+    cy.contains('h1', 'TAT CSC - Privacy Policy').should('be.visible')
+    cy.contains('p', 'Talking About Testing').should('be.visible')
+  })
 })
